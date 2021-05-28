97fbd186b2912e3c4d60fd0ceac69dbc4939ad99
JRGCast <joao.ralph1@gmail.com>
2021-05-28 11:03:19 -0300

    adicionada navBar e firulinhas

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Routes from './Routes/Routes';
 function App() {
   return (
     <main>
-<nav>
+<nav className='navBar'>
   <Link to='/'>Home</Link>
   <Link to='/curr-adv'>Curriculum Advogado</Link>
   <Link to='/curr-dev'>Curriculum Desenvolvedor</Link>
